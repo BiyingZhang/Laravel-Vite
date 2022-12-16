61b1cb091c8bd2c156e63699c26d8be7200624e5
Biying Zhang <biz11@pitt.edu>
2022-12-16 11:19:37 -0500

    Added vue-router and simple menu bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
-// require('./bootstrap');
-
 import { createApp } from 'vue'
-
+import { createRouter, createWebHistory } from 'vue-router'
 import app from './components/app.vue'
+import intro from './components/intro.vue'
 
-createApp(app).mount("#app")
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/intro', name: 'intro', component: intro }
+    ]
+})
+
+createApp(app)
+.use(router)
+.mount("#app")
--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="flex justify-center">
-    <h1 class="flex text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="bg-gray-800">
+    <div class="w-full text-white bg-main-color">
+      <div class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+        <div class="p-4 flex flex-row items-center justify-between">
+          <span class="text-3xl font-semibolde">Biying Zhang</span>
+        </div>        
+      </div>
+    </div>
+    <router-view>
+    </router-view>
+
+    <div class="grid grid-cols-4 gap-4">
+      <intro />
+      <div class="text-white">
+        panel
+      </div> 
+    </div>
   </div>
 </template>
 <script>
+
+import intro from './intro.vue'
+
 export default {
   name: "App",
+  components: { intro }
 };
 </script>
